2165f555b0fed2bc76f46d011d4e7c9d31d1d2ca
Nkepers <nick.kepers.coder@gmail.com>
2022-02-15 20:37:40 -0700

    added some initial query/mutations for users

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,28 @@
 const {gql} = require('apollo-server-express')
 
+const typeDefs = gql`
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    loginUser(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedDates: [Date]
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+`;
 
 module.exports = typeDefs
