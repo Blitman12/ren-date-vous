3fdb1966d622392d04c4c2feddc95b1220ce2471
jenmlucas <jenmlucas1@gmail.com>
2022-02-16 17:10:11 -0700

    added categories

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { makeStyles } from "@mui/styles";
+import Typography from "@mui/material/Typography";
 
+const useStyles = makeStyles({
+  title: {
+    color: "#A87901",
+    fontSize: "50px",
+    textAlign: "center",
+    fontWeight: "bold",
+    marginTop: 60
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+    alignContent: "center",
+    padding: 50,
+  },
+  buttons: {
+    width: "30%",
+    marginTop: "80px",
+    fontSize: "20px",
+    border: "solid",
+  },
+});
 
-const Categories = () => {
-    return (
-        <div>
-            Categories
-        </div>
-    )
+export default function Categories() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Typography className={classes.title}> Select A Category </Typography>
+      <Box className={classes.buttonContainer}>
+        <Button className={classes.buttons}> Home </Button>
+        <Button className={classes.buttons}> Outdoors </Button>
+        <Button className={classes.buttons}> Indoors </Button>
+        <Button className={classes.buttons}> Wild </Button>
+        <Button className={classes.buttons}> Physical </Button>
+        <Button className={classes.buttons}> Romantic </Button>
+      </Box>
+    </div>
+  );
 }
-
-export default Categories;
