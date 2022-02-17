0abce078a772510ff1628d12fcf9669b2c9cd452
lukejohnsen <johnsen.luke@gmail.com>
2022-02-17 12:48:14 -0700

    date model

---
+++ server/models/Date.js
@@ -0,0 +1,26 @@
+const { Schema } = require('mongoose');
+
+const dateSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    categories: [
+        {
+        type: String,
+        required: true,
+    },
+],
+    image: {
+        // path: String,
+    },
+    externalLink: {
+        // path: String,
+    },
+});
+
+module.exports = dateSchema;
