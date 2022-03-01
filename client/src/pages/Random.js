0950f3fc6213d157aad0fb84b007c504368859cb
B-Litty <bradlitman94@gmail.com>
2022-03-01 09:44:29 -0700

    random date rerenders with button click

--- client/src/pages/Random.js
+++ client/src/pages/Random.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@mui/styles";
 import { Button } from "@mui/material";
 import Stars from "../assets/stars.png";
@@ -15,36 +15,53 @@ const useStyles = makeStyles({
     display: 'flex',
     justifyContent: 'center',
     marginTop: '20px'
-},
-imageContainer: {
-  alignItems: "center",
-  justifyContent: "center",
-  width: "100%",
-  backgroundImage: `url(${Stars})`,
-  backgroundPosition: "center",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  height: "100vh",
-  position: "absolute",
-}
+  },
+  imageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    backgroundImage: `url(${Stars})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+    position: "absolute",
+  }
 });
 
 export default function SingleDateRandom() {
   const classes = useStyles();
   const { loading, error, data, refetch } = useQuery(DATES, {
     fetchPolicy: 'network-only',
-});
-  if(loading) return null;
-  if(error) return `error: ${error}`;
-  const {title, description, image, _id} = data.dates[Math.floor(Math.random() * data.dates.length)]
-  
+  });
+  const [currentDate, setCurrentDate] = useState()
+
+  useEffect(() => {
+    if (!loading && data) {
+      const allDates = data.dates
+      const randomDate = allDates[Math.floor(Math.random() * allDates.length)]
+      setCurrentDate(randomDate)
+    }
+  }, [loading, data])
+
+
+  const handleRandomDate = () => {
+    if (currentDate) {
+      const {title, description, image, _id} = currentDate
+      return <Date title={title} description={description} image={image} key={_id} id={_id} refetch={refetch}></Date>
+    }
+  }
+
+  if (loading) return null;
+  if (error) return `error: ${error}`;
+  const aRandomDate = data.dates[Math.floor(Math.random() * data.dates.length)]
 
   return (
     <div className={classes.imageContainer}>
-      <h1 className={classes.title}>{title}</h1>
-      <Date title={title} description={description} image={image} key={_id} id={_id} refetch={refetch}></Date>
+      <h1 className={classes.title}>Random Date</h1>
+      {handleRandomDate()}
       <div className={classes.randomButton}>
-        <Button variant="contained" size="large" onClick={() => window.location.reload()}>
+        <Button variant="contained" size="large" onClick={() => setCurrentDate(aRandomDate)}>
           Random
         </Button>
       </div>
